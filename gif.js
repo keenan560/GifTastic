de5fdfefce687eb2b1bb0c23af5cda49c6cc0430
keenan560 <keenan560@gmail.com>
2019-05-30 12:01:45 -0400

    fixed the issue with new buttons not calling to the API, added a clear button, added static sound, working on shutter sound for clear button

--- gif.js
+++ gif.js
@@ -1,10 +1,10 @@
 // Initial topics array
 
-var topics = "red blue green yellow white purple brown black indigo gray pink orange dog cat bird owl monkey moon sun fish sea ocean".split(" ");
+var topics = ["The Office", "Mad Men", "The Walking Dead", "Simpsons", "South Park", "Doug", "Fresh Prince", "Martin", "How to Get Away With Murder"];
 
 function renderArray(topics) {
     for (var i = 0; i < topics.length; i++) {
-        var newBtn = $("<button class='bg-secondary gif btn btn-lg text-white mx-2 mb-3 mt-3'>").html(topics[i]);
+        var newBtn = $("<button class='bg-dark gif btn btn-lg text-white mx-2 mb-3 mt-3'>").html(topics[i]);
         $(newBtn).attr("value", topics[i]);
         $("#array").append(newBtn);
 
@@ -15,6 +15,7 @@ var btnVal;
 
 function apiCall() {
     $("#results").empty();
+    playAudio(channel);
     btnVal = $(this).val().trim();
     console.log(btnVal);
     queryURL = "https://api.giphy.com/v1/gifs/search?api_key=A5gG6PiSqmi9arWHKGFdGS2Vw9nJQkwa&q=" + btnVal + "&limit=10&rating=R&lang=en"
@@ -26,16 +27,29 @@ function apiCall() {
         console.log(response);
 
         for (var i = 0; i < response.data.length; i++) {
-            var gif = $("<img height='200' width='200' border='3px' class='gifs .img-thumbnail' type='video/mp4'>response.data[i].rating");
+            var gif = $("<img height='200' width='200' border='3px' class='gifs img-thumbnail' type='video/mp4'>");
             $(gif).attr("src", response.data[i].images.original.url);
             $("#results").append(gif);
+            // $("#results").append("Rated: " + response.data[i].rating).attr("class", 'rating');
         }
     });
 }
+//Code to clear results 
+function clear() {
+    $("#results").empty();
+    $("#array").empty();
+    playAudio(shutter);
+    renderArray();
+}
+//Code for TV Static MP3 used when you click on a TV Show button 
+var channel = new Audio("tv-static-01.mp3");
 
+var shutter = new Audio("Camera-shutter-sound.mp3");
 
+let playAudio = function (sound) {
+    sound.play();
+}
 // OnClick event for buttons 
-
 
 
 $(document).ready(function () {
@@ -43,17 +57,18 @@ $(document).ready(function () {
     renderArray(topics);
 
     $("#submit").on("click", function (event) {
-        $("#results").empty();
         event.preventDefault();
         btnVal = $("#input-topic").val();
         console.log(btnVal);
-        var newBtn = $("<button class='gif btn btn-lg bg-secondary text-white mx-2 mb-3 mt-3'onclick='apiCall()'>").html(btnVal);
+        var newBtn = $("<button class='btn gif btn-lg bg-dark text-white mx-2 mb-3 mt-3'>").html(btnVal);
         $(newBtn).attr("value", btnVal);
         $("#array").append(newBtn);
 
     })
 
-    $(".gif").on("click", apiCall);
+    $(document).on("click", "#clear", clear);
+
+    $(document).on("click", ".gif", apiCall);
 
 })
 
